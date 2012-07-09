c11af8428b7cbf4af2a30f27f89d496896d82fb7
Jonathan Hamshaw <jonathan.hamshaw@mckinsey.com>
2012-07-09 19:18:02 +0100

    added client and server console executables

---
+++ Client/Client.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Client
+{
+    class Client
+    {
+        static void Main()
+        {
+            try
+            {
+                var ipAddress = "172.20.83.18";
+                const int port = 8001;
+
+                var tcpClient = new TcpClient();
+                Console.WriteLine("Connecting...");
+                
+                tcpClient.Connect(ipAddress, port);
+
+                Console.WriteLine("Connected");
+                Console.Write("Enter the string to be transmitted : ");
+
+                var str = Console.ReadLine();
+                Stream stream = tcpClient.GetStream();
+
+                var asciiEncoding = new ASCIIEncoding();
+                var bytes = asciiEncoding.GetBytes(str);
+                Console.WriteLine("Transmitting...");
+
+                stream.Write(bytes, 0, bytes.Length);
+
+                var bytes2 = new byte[100];
+                var k = stream.Read(bytes2, 0, 100);
+
+                for (var i = 0; i < k; i++)
+                {
+                    Console.Write(Convert.ToChar(bytes2[i]));
+                }
+
+                tcpClient.Close();
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine("Error......." + e.StackTrace);
+            }
+        }
+    }
+}
---
+++ TcpTalk/Server.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Server
+{
+    public class Server
+    {
+        private static void Main()
+        {
+            try
+            {
+                var ipAddress = IPAddress.Parse("172.20.83.18");
+                const int port = 8001;
+
+                //Initialises the listener
+                var listener = new TcpListener(ipAddress, port);
+
+                //Start listening to the specified port
+                listener.Start();
+
+                Console.WriteLine("The server is running at port {0}...", port);
+                Console.WriteLine("The local endpoint is : {0}", listener.LocalEndpoint);
+                Console.WriteLine("Waiting for a connection...");
+
+                Socket socket = listener.AcceptSocket();
+                Console.WriteLine("Connection accepted from {0}", socket.RemoteEndPoint);
+
+                var bytes = new byte[100];
+                var k = socket.Receive(bytes);
+                Console.WriteLine("Received...");
+                for (int i = 0; i < k; i++)
+                {
+                    Console.Write(Convert.ToChar(bytes[i]));
+                }
+
+                var asciiEncoding =  new ASCIIEncoding();
+                socket.Send(asciiEncoding.GetBytes("The string was received by the server."));
+                Console.WriteLine("\nSent Acknowledgement");
+
+                //Clean up
+                socket.Close();
+                listener.Stop();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("There was an error.......!" + e.StackTrace);
+            }
+        }
+    }
+}
